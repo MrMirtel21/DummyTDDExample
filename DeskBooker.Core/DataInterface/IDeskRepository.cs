f884450a2f13339c47e056d2c5e4b707767b4588
MrMirtel21 <93584447+MrMirtel21@users.noreply.github.com>
2023-11-06 16:31:55 +0100

    feat: added data access layer and presentation layer

--- DeskBooker.Core/DataInterface/IDeskRepository.cs
+++ DeskBooker.Core/DataInterface/IDeskRepository.cs
@@ -1,9 +1,12 @@
 ﻿using DeskBooker.Core.Domain;
+using System;
+using System.Collections.Generic;
 
 namespace DeskBooker.Core.DataInterface
 {
-    public interface IDeskRepository
-    {
-        IEnumerable<Desk> GetAvailableDesks(DateTime date);
-    }
+  public interface IDeskRepository
+  {
+    IEnumerable<Desk> GetAvailableDesks(DateTime date);
+    IEnumerable<Desk> GetAll();
+  }
 }
